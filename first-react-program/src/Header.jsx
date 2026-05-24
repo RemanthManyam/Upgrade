8adfa138eb21832328556c15356dee01a712da42
Remanth Manyam <manyamremanth@gmail.com>
2026-05-24 15:31:16 +0000

    Built the Header. Moving on to Foooter

---
+++ first-react-program/src/Header.jsx
@@ -0,0 +1,22 @@
+function Header(){
+
+    return(
+        <header>
+            <h1> First Contact with React <img src="react.svg" alt="logo of react"></img> </h1>
+            
+            <nav>
+                <h4>Terminology in React:</h4>
+                <ul>
+                    <li><a href="#">Components</a></li>
+                    <li><a href="#">Vite</a></li>
+                    <li><a href="#">JavaScript XML</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+                
+            
+        </header>
+    );
+}
+
+export default Header
