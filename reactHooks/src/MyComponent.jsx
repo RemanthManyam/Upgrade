e5c84e5a20c0ad36a648aa756be94fff00732482
Remanth Manyam <manyamremanth@gmail.com>
2026-05-31 09:32:28 +0000

    First React Hooks program. A Simple ChangeName Program

--- reactHooks/src/MyComponent.jsx
+++ reactHooks/src/MyComponent.jsx
@@ -1,6 +1,28 @@
 import React, {useState} from 'react';
 
 function MyComponent(){
-    return(<p>Hi</p>);
+
+    // const [age, setAge] = useState(0);
+
+    // const incrementAge = () => {
+    //     setAge(age + 1);
+    // }
+
+    // return(<div>
+    //     <p>Age: {age}</p>
+    //     <button onClick={incrementAge}>Increase Age</button>
+    // </div>);
+
+    const [name, setName] = useState("Guest");
+
+    const updateName = () => {
+        setName("Cadence");
+    }
+
+    return(<div>
+        <p>Name: {name}</p>
+        <button onClick = {updateName}>Update Name</button>
+    </div>)
+
 }
 export default MyComponent;
