1e67d1dfb6c2ce94b57c4d6aa3b255fbbd38c897
Remanth Manyam <manyamremanth@gmail.com>
2026-05-31 19:29:52 +0000

    Counter program with UseEffect

--- reactHooks/src/UseEffect.jsx
+++ reactHooks/src/UseEffect.jsx
@@ -1,7 +1,28 @@
 import React, {useState, useEffect} from 'react';
 
 function UseEffect(){
-    return(<></>);
+
+    const[count, setCount] = useState(0);
+
+    useEffect(() => {                                       //useEffect(function, [dependencies])
+        document.title = `${count}`;
+    },[count]);                   
+
+    function inc(){
+        setCount(c => c + 1);
+    }
+
+    function dec(){
+        setCount(c => c - 1);
+    }
+
+    return(<>
+    <p>Count: {count}</p>
+    <button onClick = {inc}>+</button>
+    <button onClick = {dec}>-</button>
+    
+    </>);
+
 }
 
 export default UseEffect;
