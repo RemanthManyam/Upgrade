c1bd633b21d1892b4514fb8ce4b9428d36b34c02
Remanth Manyam <manyamremanth@gmail.com>
2026-05-31 19:40:55 +0000

    Set up Codebase for 'cleaning up using Use Effect' sss

--- reactHooks/src/App.jsx
+++ reactHooks/src/App.jsx
@@ -1,9 +1,10 @@
 import Counter from "./Counter.jsx";
 import UseState from './UseState.jsx'
 import UseEffect from './UseEffect.jsx'
+import UseEffectForCleaningUp from "./UseEffectForCleaningUp.jsx";
  
 function App(){
-  return(<UseEffect></UseEffect>);
+  return(<UseEffectForCleaningUp></UseEffectForCleaningUp>);
 }
 
 export default App
