07cfd3774590437881a1add83fc2e306e0eb0678
Remanth Manyam <manyamremanth@gmail.com>
2026-05-31 20:06:57 +0000

    Used UseEffect for CleaningUp and reducing the number of EventListeners used

--- reactHooks/src/UseEffectForCleaningUp.jsx
+++ reactHooks/src/UseEffectForCleaningUp.jsx
@@ -2,8 +2,34 @@ import React, {useState, useEffect} from 'react';
 
 function UseEffectForCleaningUp(){
 
+    const [height, setHeight] = useState(window.innerHeight);
+    const [width, setWidth] = useState(window.innerWidth);
 
-    return(<></>);
+    useEffect(() => {
+        window.addEventListener("resize", dynamic);
+        console.log("EVENT LISTENER ADDED");
+
+        return() => {window.removeEventListener("resize", dynamic)
+            console.log("EVENT LISTENER REMOVED");
+        }
+},[])
+
+useEffect(() => {
+    document.title = `Size: ${height} x ${width}`;
+},[height,width])
+
+    
+
+    function dynamic() {
+        setHeight(window.innerHeight);
+        setWidth(window.innerWidth);
+    }
+
+
+    return(<>
+    <p>Height: {height}px</p>
+    <p>Width: {width}px</p>
+    </>);
 }
 
 export default UseEffectForCleaningUp;
