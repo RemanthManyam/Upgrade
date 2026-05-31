9da20daf34c28417003c7c0758d343bad2bf2677
Remanth Manyam <manyamremanth@gmail.com>
2026-05-31 19:35:16 +0000

    UseEffect has 3 cases: No dependecies, empty dependencies[], with dependencies. This is 3rd one

--- reactHooks/src/UseEffect.jsx
+++ reactHooks/src/UseEffect.jsx
@@ -3,10 +3,11 @@ import React, {useState, useEffect} from 'react';
 function UseEffect(){
 
     const[count, setCount] = useState(0);
+    const[colour, setColor] = useState("blue");
 
     useEffect(() => {                                       //useEffect(function, [dependencies])
-        document.title = `${count}`;
-    },[count]);                   
+        document.title = `${count} ${colour}`;
+    },[count, colour]);                   
 
     function inc(){
         setCount(c => c + 1);
@@ -16,11 +17,15 @@ function UseEffect(){
         setCount(c => c - 1);
     }
 
+    function changeColor(){
+        setColor(c => c === "blue" ? "green" : "blue")
+    }
+
     return(<>
-    <p>Count: {count}</p>
+    <p style = {{color: colour}}>Count: {count}</p>
     <button onClick = {inc}>+</button>
-    <button onClick = {dec}>-</button>
-    
+    <button onClick = {dec}>-</button><br/>
+    <button onClick= {changeColor}>Change Color</button>
     </>);
 
 }
