0604edde4e9d4cb98355189f1df33d85c2665ce2
Remanth Manyam <manyamremanth@gmail.com>
2026-05-31 09:51:54 +0000

    Completed the basic React Hooks program

--- reactHooks/src/MyComponent.jsx
+++ reactHooks/src/MyComponent.jsx
@@ -14,14 +14,30 @@ function MyComponent(){
     // </div>);
 
     const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
+    const [isEmployed, empoloymentStatus] = useState(false);
 
     const updateName = () => {
         setName("Cadence");
     }
 
+    const increaseAge = () => {
+        setAge(age + 1);
+    }
+
+    const employToggler = () => {
+        empoloymentStatus(!(isEmployed));
+    }
+
     return(<div>
         <p>Name: {name}</p>
         <button onClick = {updateName}>Update Name</button>
+
+        <p>Age: {age}</p>
+        <button onClick = {increaseAge}>+</button>
+
+        <p>Employment Status: {isEmployed ? "true" : "false"}</p>
+        <button onClick = {employToggler}>Toggle Employment</button>
     </div>)
 
 }
