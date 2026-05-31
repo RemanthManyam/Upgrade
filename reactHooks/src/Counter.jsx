b0c5afb647635e02992185d1d62521090d466494
Remanth Manyam <manyamremanth@gmail.com>
2026-05-31 10:24:07 +0000

    Done with the Counter project

--- reactHooks/src/Counter.jsx
+++ reactHooks/src/Counter.jsx
@@ -2,7 +2,26 @@ import React, {useState} from 'react';
 
 function Counter(){
 
-    return(<></>);
+    const [count, changeCount] = useState(0);
+
+    const increase = () => {
+        changeCount(count + 1);
+    }
+
+    const decrease = () => {
+        changeCount(count - 1);
+    }
+
+    const reset = () => {
+        changeCount(0);
+    }
+
+    return(<div className="body">
+        <p className="counter">{count}</p>
+        <button className="countButton" onClick={increase}>+</button>
+        <button className="countButton" onClick={reset}>Reset</button>
+        <button className="countButton" onClick={decrease}>-</button>
+    </div>);
 }
 
 export default Counter
