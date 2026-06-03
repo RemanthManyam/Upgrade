0d4e307dac0ffdd9d89c8f3f55188956b83b6da1
Remanth Manyam <manyamremanth@gmail.com>
2026-06-03 19:29:41 +0000

    Tough but coded List Rendering with Props

--- props-conditional-and-list-rendering/src/App.jsx
+++ props-conditional-and-list-rendering/src/App.jsx
@@ -5,9 +5,13 @@ import ListRenderingWithProps from './ListRenderingWithProps.jsx'
 
 function App(){
 
-  return(<>
-  <ListRenderingWithProps></ListRenderingWithProps>
-  </>);
+  const games = [{uid: 1, name: "Elden Ring", copiesSold: 30}, 
+                {uid: 2, name: "Red Dead", copiesSold: 165}, 
+                {uid: 3, name: "Clair Obscur", copiesSold: 7}, 
+                {uid: 4, name: "Outlast", copiesSold: 11}, 
+                {uid: 5, name: "Half Life 2", copiesSold: 1}];
+
+  return(<ListRenderingWithProps arr={games} title="Games"></ListRenderingWithProps> );
 }
 
 export default App;
--- props-conditional-and-list-rendering/src/ListRenderingWithProps.jsx
+++ props-conditional-and-list-rendering/src/ListRenderingWithProps.jsx
@@ -1,8 +1,17 @@
+import { array } from "prop-types";
 
 
-function ListRenderingWithProps(){
+function ListRenderingWithProps(props){
 
-    return(<>Hi</>);
+    const title = props.title;
+    const arr = props.arr
+
+    const list = arr.map((ele) => <li key={ele.id}>
+                                     {ele.name}: &nbsp;<br></br>
+                                    Copies Sold: <i>{ele.copiesSold}</i>M
+    </li>)
+
+    return(<><h3>{title}</h3> <ol>{list}</ol></>);
 }
 
 export default ListRenderingWithProps;
