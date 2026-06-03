2583b4c3fc5030a85c9d51a757b9b08103742a2b
Remanth Manyam <manyamremanth@gmail.com>
2026-06-03 18:06:39 +0000

    Basic List Rendering program

--- props-conditional-and-list-rendering/src/ListRendering.jsx
+++ props-conditional-and-list-rendering/src/ListRendering.jsx
@@ -2,7 +2,12 @@
 
 function ListRendering(){
     
-    return(<></>);
+    const games = ["Elden Ring", "Red Dead", "Clair Obscur", "Outlast", "Half Life 2"]
+
+    // games.sort();
+
+    const listItems = games.map((ele) => <li>{ele}</li>);
+    return(<ul>{listItems}</ul>);
 }
 
 export default ListRendering;
