4eeeb4de878b2e4442f9c066d294919c74e1187b
Remanth Manyam <manyamremanth@gmail.com>
2026-06-06 20:40:53 +0000

    Cleanup

--- to-do-list/src/ToDo.jsx
+++ to-do-list/src/ToDo.jsx
@@ -2,7 +2,23 @@
 
 function ToDo(){
 
-    return(<><>HI</></>);
+    const arr = [];
+
+    a button(btn)
+    a inputbox(userInput)
+
+    btn.onClick(() => {
+
+        arr.push(userInput);
+        userInput.clear();
+
+    })
+
+    let task = arr.map((ele) => {
+        return ele;
+    })
+
+    return(<><li>{task}</li></>);
 }
 
 export default ToDo;
