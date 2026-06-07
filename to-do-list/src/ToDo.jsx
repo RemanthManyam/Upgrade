9186376e6ce57aafca5fc8f462db97bc4b0c816d
Remanth Manyam <manyamremanth@gmail.com>
2026-06-07 00:43:50 +0000

    Done with basic ToDo List

--- to-do-list/src/ToDo.jsx
+++ to-do-list/src/ToDo.jsx
@@ -5,13 +5,18 @@ function ToDo(){
     const [track, setTrack] = useState();
     const [task, newTask] = useState([]);
 
+    const handleText = () => {
+        if(track.trim() !== ""){
+            newTask([...task, track]);
+            setTrack('');
+        }
+    }
+
     return(<>
     <h2>To-Do-list</h2>
-    <input type="text" placeholder="Enter a task..."></input> 
-    <button>Add</button>
-    <ol><li>Hy</li>
-        <li>Hy</li>
-        <li>Hy</li></ol>
+    <input type="text" placeholder="Enter a task..." onChange={(e) => setTrack(e.target.value)}></input> 
+    <button onClick={handleText}>Add</button>
+    <ul><li>{task.join("|")}</li></ul>
     </>);
 }
 
